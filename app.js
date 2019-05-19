49b0aeab1ddb88581335d8a016cbd78bd46a8deb
riyadh <riyadh02220@gmail.com>
2019-05-20 00:44:11 +0200

    sonic speed

--- app.js
+++ app.js
@@ -5,15 +5,19 @@ let degree = 5;
 /* play with the stars*/
 for(let i=1; i<=29; i++){
     stars.childNodes[i].style.color = "green";
-    if( (i>=8 && i<=10) || (i>=20 && i<=22) || i=== 14 || i===16)  {
-        stars.childNodes[i].style.color = "blue";
-    }
+    
     setInterval(()=> {
         setTimeout(()=> {
             stars.childNodes[i].style.transform = 'rotate('+degree+'deg)';
             degree+=5;
-        },500)
-    },1000)
+        },100)
+    },110)
+    stars.childNodes[1].style.transform = 'rotate('+degree+'deg)';
+
+
+    if( (i>=8 && i<=10) || (i>=20 && i<=22) || i=== 14 || i===16)  {
+        stars.childNodes[i].style.color = "blue";
+    }
 }
 
 
