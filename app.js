debaec525c571f47ebd8b6b71172608216509161
riyadh <riyadh02220@gmail.com>
2019-05-20 19:47:07 +0200

    add ready go box

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // GET IN THE DOM :
 const stars = document.querySelector("main");
+const welcome = document.querySelector("h1");
 let degree = 0.1;
 
 // FUCTIONS ////////////////////////////////////////
@@ -7,7 +8,7 @@ let degree = 0.1;
 /* mini star */
 function miniStar(ev) {
     let x = ev.clientX /2;
-    let y = ev.clientY /2
+    let y = ev.clientY /2;
 
     ev.target.style.color = "rgb("+x+","+y+",80)";
     ev.target.style.fontSize = "3rem";
@@ -27,19 +28,51 @@ function rotate(i,speed) {
     },100)
 }
 
+/* show the ready go box */
+
+
+
+
+
+
 // PLAY WITH THE STARS ///////////////////////////
 
-for(let i=1; i<=29; i++){
-    stars.addEventListener("mouseover",(ev)=> {
-        miniStar(ev)
-        rotate(i,5)
-    })
-    stars.addEventListener("mouseout",(ev)=> {
-        maxiStar(ev)
-    })
-    rotate(i,1);
-    
 
-}
+
+    setTimeout(()=> {
+        welcome.style.visibility = "visible";
+    },500)
+    setTimeout(()=> {
+        welcome.style.visibility = "hidden";
+    },1000)
+    setTimeout(()=> {
+        welcome.style.visibility = "visible";
+        welcome.textContent ="GO";
+    },1500)
+    setTimeout(()=> {
+        welcome.style.visibility = "hidden";
+    },2000)
+
+    setTimeout(()=> {
+        for(let i=1; i<=29; i++){
+            stars.addEventListener("mouseover",(ev)=> {
+                miniStar(ev)
+                rotate(i,5)
+            })
+            stars.addEventListener("mouseout",(ev)=> {
+                maxiStar(ev)
+            })
+                rotate(i,1);
+        }
+    },2500)
+
+    setTimeout(()=> {
+        welcome.style.visibility = "visible";
+        welcome.textContent ="HOVER ME";
+    },6000)
+    setTimeout(()=> {
+        welcome.style.visibility = "hidden";
+    },6500)
+
 
 
